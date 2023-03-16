6d03a4fe13a3c9dd804f7b6d5eeb6a081b2f7a23
HarrisonSteer1 <117164747+HarrisonSteer1@users.noreply.github.com>
2023-03-16 14:36:47 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-docker rmi $(docker images -a -q)
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-cd dockerfileexercise/Task1
-docker build -t task1 . 
+docker build -t task1 dockerfileexercise/Task1 
