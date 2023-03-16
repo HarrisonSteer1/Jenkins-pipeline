529fdcc461708909ba3ed3e6249aade8c3f095ff
HarrisonSteer1 <117164747+HarrisonSteer1@users.noreply.github.com>
2023-03-16 14:25:05 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-sudo docker rmi $(docker images -a -q)
-sudo docker stop $(docker ps -a -q)
-sudo docker rm $(docker ps -a -q)
+docker rmi $(docker images -a -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 cd dockerfileexercise/Task1
-sudo docker build -t task1 . 
+docker build -t task1 . 
